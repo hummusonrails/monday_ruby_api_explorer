a2561e8fbba8ea363ab1ba304117ac15023c23e4
benhayehudi <bengreenberg@gmail.com>
2020-08-18 10:45:21 +0300

    reorganize subscribers class

--- lib/monday/boards.rb
+++ lib/monday/boards.rb
@@ -7,11 +7,7 @@ class Monday::Boards
     Monday::Mutations::Boards.new.archive_board(board_id)
   end
 
-  def add_subscribers
-    Monday::Mutations::Boards::Subscribers.new.add_subscribers(params = {})
-  end
-
-  def delete_subscribers
-    Monday::Mutations::Boards::Subscribers.new.delete_subscribers(params = {})
+  def subscribers
+    Monday::Mutations::Boards::Subscribers.new
   end
 end
